33347a603410356a97bd8576a83c415d4647a745
gka <gka-github@mailbox.org>
2024-01-17 12:50:29 +0000

    make loadData work on localhost

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ import { csv } from 'd3-fetch';
 import { autoType } from 'd3-dsv';
 
 export function loadData(url:string) {
-    return csv(`${location.host !== 'vis4.net' ? '/src/assets' : '/blog/interactives/2401-svelteplot-demos/data'}/${url}`, autoType);
+    return csv(`${location.host.endsWith('.webcontainer.io') ? '/src/assets' : '/blog/interactives/2401-svelteplot-demos/data'}/${url}`, autoType);
 }
